b2df80a4fd35ac1c1a233cb761eb8e0f529522d5
mimura <musyu36@gmail.com>
2020-03-03 22:33:08 +0900

    フォルダ最大文字数を設定

--- app/src/main/java/com/example/photopractice/AddFolderActivity.kt
+++ app/src/main/java/com/example/photopractice/AddFolderActivity.kt
@@ -1,16 +1,26 @@
 package com.example.photopractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.InputFilter.LengthFilter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_folder.*
 
+
 class AddFolderActivity : AppCompatActivity() {
+
+    companion object{
+        val MAX_FOLDER_LENGTH = 15
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_folder)
 
+        editFolderName
+        val lengthFilter = LengthFilter(MAX_FOLDER_LENGTH)
+        editFolderName.setFilters(arrayOf<InputFilter>(lengthFilter))
 
         //保存
         btnSaveFolder.setOnClickListener{
